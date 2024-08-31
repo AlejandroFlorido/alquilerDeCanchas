3a9eb386a032665e4e6b03230091e770bebd7f81
José Nicolás Núñez <nunezjosenicolas4598@gmail.com>
2024-08-31 11:10:09 -0300

    Actualizacion para la carga de datos
    Se Actualizo el super Usuario
    usuario: usuario
    contaseña: usuario

--- AlquilerDeCanchas/AlquilerDeCanchas/settings.py
+++ AlquilerDeCanchas/AlquilerDeCanchas/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'AlquilerDeCanchas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'SistemaDeAlquilerDeCanchas.db',
     }
 }
 
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
--- AlquilerDeCanchas/GestionDeCanchas/models.py
+++ AlquilerDeCanchas/GestionDeCanchas/models.py
@@ -2,87 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Cancha:
-    def __init__(self, id_cancha, tipo, disponible=True, con_luz=False, horario_reservado=None, duracion_reserva=1) -> None:
-        self.id_cancha = id_cancha
-        self.tipo = tipo
-        self.disponible = disponible
-        self.con_luz = con_luz
-        self.horario_reservado = horario_reservado
-        self.duracion_reserva = duracion_reserva
+class Cancha(models.Model):
 
-    def calcular_horario_fin(self):
-        if self.horario_reservado:
-            hora_inicio, _ = map(int, self.horario_reservado.split(":"))
-            hora_fin = (hora_inicio + self.duracion_reserva) % 24
-            return f"{hora_fin:02}:00"
-        return None
-
-    def __str__(self):
-        if self.disponible:
-            estado = "Disponible"
-        else:
-            horario_fin = self.calcular_horario_fin()
-            estado = f"Ocupado (Reservada de {self.horario_reservado} a {horario_fin})"
-        luz = "con luz" if self.con_luz else ""
-        return f"Cancha {self.id_cancha}: {self.tipo} - {estado} {luz}".strip()
-
-class SistemaReservas:
-    def __init__(self):
-        self.canchas = [
-            Cancha(1, "Básquet"), Cancha(2, "Básquet"),
-            Cancha(3, "Vóley"), Cancha(4, "Vóley"),
-            Cancha(5, "Tenis"), Cancha(6, "Tenis"),
-            Cancha(7, "Pádel"), Cancha(8, "Pádel"),
-            Cancha(9, "Pádel"), Cancha(10, "Pádel"),
-            Cancha(11, "Fútbol 5"), Cancha(12, "Fútbol 5")
-        ]
-        self.reservas = []
-
-    def mostrar_canchas_por_horario(self, horario):
-        for cancha in self.canchas:
-            print(cancha)
-
-    def mostrar_canchas_ocupadas(self):
-        canchas_ocupadas = [c for c in self.canchas if not c.disponible]
-        if canchas_ocupadas:
-            print("\nCanchas ocupadas:")
-            for cancha in canchas_ocupadas:
-                print(cancha)
-        else:
-            print("No hay canchas ocupadas en este momento.")
-
-    def reservar_cancha(self, id_cancha, horario, duracion=1, con_luz=False):
-        cancha = next((c for c in self.canchas if c.id_cancha == id_cancha), None)
-        if cancha and cancha.disponible:
-            cancha.disponible = False
-            cancha.con_luz = con_luz
-            cancha.horario_reservado = horario
-            cancha.duracion_reserva = duracion
-            self.reservas.append(cancha)
-            hora_fin = cancha.calcular_horario_fin()
-            luz = "con luz" if con_luz else ""
-            print(f"Reserva exitosa para {cancha.tipo} (ID: {id_cancha}) de {horario} a {hora_fin} {luz}")
-        else:
-            print("Cancha no disponible o inexistente.")
-
-    def liberar_cancha(self, id_cancha):
-        cancha = next((c for c in self.canchas if c.id_cancha == id_cancha), None)
-        if cancha and not cancha.disponible:
-            cancha.disponible = True
-            cancha.con_luz = False
-            cancha.horario_reservado = None
-            cancha.duracion_reserva = 1
-            self.reservas.remove(cancha)
-            print(f"La cancha {id_cancha} ha sido liberada.")
-        else:
-            print("Cancha ya está disponible o no existe.")
-
-    def horario_valido(self, horario):
-        try:
-            hora, minutos = map(int, horario.split(":"))
-            if (13 <= hora < 24) or (0 <= hora <= 2):
-                return True
-            return False
-        except ValueError:
-            return False
+    Nombre_cancha = models.CharField(max_length=50)
+    id_cancha = models.CharField(primary_key=True, max_length=6)
+    tipo = models.CharField(max_length=50)
+    disponible = models.BooleanField()
+    con_luz = models.BooleanField()
+    horario_reservado = models.BooleanField()
+    duracion_reserva = models.IntegerField()
